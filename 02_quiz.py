5a6982872cb2a1984b12d60c0ee9fc7864c2e8d8
doomness87 <louth.joshu@awatapu.school.nz>
2021-05-26 13:16:20 +1200

    Quiz done
    The quiz is done and the issue is fixed just had to put it into an elif statement

--- 02_quiz.py
+++ 02_quiz.py
@@ -1,13 +1,60 @@
-def questions(question_1, question_2, question_3, question_4, question_5):
+def questions():
     question_1 = input("When did the last of us 1 come out\n"
                        "a: 2013 "
                        "b: 2014 "
                        "c: 2010 "
                        "d: 2016")
-    if question_1 != "2013" and "a":
-        print("Sorry that is incorrect it was actually 2013")
-    else:
+    if question_1 == "a":
         print("Well done that is correct moving onto next question")
+    elif question_1 == "2013":
+        print("Well done that is correct moving onto next question")
+    else:
+        print("Sorry that is incorrect it was actually 2013")
+    question_2 = input("What does RTS stand for\n"
+                       "a: Ree Toxic Stans "
+                       "b: Real Tab Stigma "
+                       "c: Real Time Strategy "
+                       "d: Reggie Tis Sus")
+    if question_2 == "c":
+        print("Well done that is correct moving onto next question")
+    elif question_2 == "Real Time Strategy":
+        print("Well done that is correct moving onto next question")
+    else:
+        print("Sorry that is incorrect it was actually Real Time Strategy")
+    question_3 = input("What nationality is Dhalism from Street Fighter\n"
+                       "a: Bangladeshi "
+                       "b: Bhutanese "
+                       "c: Japanese "
+                       "d: Indian")
+    if question_3 == "d":
+        print("Well done that is correct moving onto next question")
+    elif question_3 == "Indian":
+        print("Well done that is correct moving onto next question")
+    else:
+        print("Sorry that is incorrect he's actually Indian")
+    question_4 = input("Where was Tetris made"
+                       "a: Russia "
+                       "b: Belarus "
+                       "c: Poland "
+                       "d: Australia")
+    if question_4 == "a":
+        print("Well done that is correct moving onto next question")
+    elif question_4 == "Russia":
+        print("Well done that is correct moving onto next question")
+    else:
+        print("Sorry that is incorrect it's actually made in Russia")
+    question_5 = input("To the closest 10 what is the score on metacritic for Batman Arkham City PS3"
+                       "a: 90 "
+                       "b: 100 "
+                       "c: 40 "
+                       "d: 70")
+    if question_5 == "b":
+        print("Well done that is correct moving onto next question")
+    elif question_5 == "100":
+        print("Well done that is correct moving onto next question")
+    else:
+        print("Sorry that is incorrect it's actually 100 did you get the wrong console?\n"
+              "Thanks for doing my quiz")
 
 
-print(questions("", "", "", "", ""))
+print(questions())
